ca76bd59bd3beaa3cbcc3eb8b348c2bd291939f4
Artyom Mikhnevich <32782411+vanholler@users.noreply.github.com>
2021-05-28 12:23:23 +0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -5,7 +5,7 @@ mongoose.set('debug', true)
 const connectDB = async () => {
   try {
     console.log('MongoDB Connecting...')
-    const conn = await mongoose.connect('mongodb+srv://mainUser:vgnns31er@based.qkobt.mongodb.net/firstDatabase?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect('mongodb+srv://<USER>:<PASS>@based.qkobt.mongodb.net/firstDatabase?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
